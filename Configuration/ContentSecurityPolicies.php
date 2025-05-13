50e259da12d8c58d70eb9258591d2e5bc46d3e56
Xavier Perseguers <xavier@causal.ch>
2025-05-13 14:19:27 +0200

    [BUGFIX] Add missing CSP definition
    Related: #62

--- Configuration/ContentSecurityPolicies.php
+++ Configuration/ContentSecurityPolicies.php
@@ -43,6 +43,11 @@ return Map::fromEntries([
         ),
         new Mutation(
             MutationMode::Append,
+            Directive::FrameSrc,
+            new UriValue('https://accounts.crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
             Directive::StyleSrc,
             new UriValue('https://fonts.googleapis.com')
         ),
