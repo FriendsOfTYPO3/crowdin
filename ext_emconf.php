cf7d604b9255f4d1b933454d75a54da076747b72
Xavier Perseguers <xavier@causal.ch>
2025-11-11 20:37:24 +0100

    [TASK] Raise dependency to support PHP 8.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.1',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.4.99',
+            'php' => '8.1.0-8.5.99',
             'typo3' => '11.5.0-13.4.99',
         ],
         'conflicts' => [],
