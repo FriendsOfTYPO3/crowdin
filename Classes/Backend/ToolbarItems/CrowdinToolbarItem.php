7f20bdd3119b7d3f025d827cc6c6d337b0d68d54
Xavier Perseguers <xavier@causal.ch>
2025-11-13 09:26:43 +0100

    [BUGFIX] Prevent useless horizontal scrolling of the Crowdin toolbar menu

--- Classes/Backend/ToolbarItems/CrowdinToolbarItem.php
+++ Classes/Backend/ToolbarItems/CrowdinToolbarItem.php
@@ -139,7 +139,12 @@ class CrowdinToolbarItem implements ToolbarItemInterface
 
         $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = $backupDebug;
 
-        return $result['html'];
+        // We need to get rid of a "form-wizards-item-element" class that is useless and
+        // breaks or hand-crafted layout by enforcing a 120px minimum width (backend.css)
+        // for the toggle usually part of a TCA form
+        $checkboxHtml = str_replace('<div class="form-wizards-item-element">', '<div>', $result['html']);
+
+        return $checkboxHtml;
     }
 
     protected function getExtensionsCompatibleWithCrowdin(): array
