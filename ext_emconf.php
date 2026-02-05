ecaa80c12696f3998bac7edf089d98dedee4c0ac
Xavier Perseguers <xavier@causal.ch>
2026-02-05 22:51:04 +0100

    [TASK] Raise TYPO3 v14 supported version

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.1.0-8.5.99',
-            'typo3' => '12.4.0-14.0.99',
+            'typo3' => '12.4.0-14.1.99',
         ],
         'conflicts' => [],
         'suggests' => [],
