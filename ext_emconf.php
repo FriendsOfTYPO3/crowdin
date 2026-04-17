e0dec7a23e94483c1e90144c359fa5e079d383b6
Xavier Perseguers <xavier@causal.ch>
2026-04-17 09:02:41 +0200

    [TASK] Restore compatibility with PHP 8.1
    Usage of readonly class is only bound to TYPO3 v14 where
    minimum PHP requirement is PHP 8.2. Compatibility with
    TYPO3 v12 and PHP 8.1 is thus not affected.
    
    Related: #81

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.1',
     'constraints' => [
         'depends' => [
-            'php' => '8.2.0-8.5.99',
+            'php' => '8.1.0-8.5.99',
             'typo3' => '12.4.0-14.3.99',
         ],
         'conflicts' => [],
